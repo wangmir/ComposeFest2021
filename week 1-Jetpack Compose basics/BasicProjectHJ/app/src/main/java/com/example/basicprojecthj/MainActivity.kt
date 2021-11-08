3211029bdb51f36a816ea43f50c8848e2d60ad7d
Hyukjoong Kim <wangmir@gmail.com>
2021-11-08 22:25:50 +0900

    Add expanded button.
    Signed-off-by: Hyukjoong Kim <wangmir@gmail.com>

--- week 1-Jetpack Compose basics/BasicProjectHJ/app/src/main/java/com/example/basicprojecthj/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicProjectHJ/app/src/main/java/com/example/basicprojecthj/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(names: List<String> = listOf("Hyukjoong", "Compose")) {
-    Column (modifier = Modifier.padding(vertical = 4.dp)) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -39,18 +43,27 @@ private fun MyApp(names: List<String> = listOf("Hyukjoong", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+    val padding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier
-            .padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+        ) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = padding)
+            ) {
                 Text(text = "Hello")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = expanded.value.not() }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
 
@@ -61,7 +74,7 @@ private fun Greeting(name: String) {
 @Composable
 fun MyAppPreview() {
     BasicProjectHJTheme {
-       MyApp()
+        MyApp()
     }
 }
 
