9e58aec0c9867d70a506c75fc0d7ce2089de8464
Hyukjoong Kim <wangmir@gmail.com>
2021-11-21 21:14:49 +0900

    Intrinsics handling.
    Signed-off-by: Hyukjoong Kim <wangmir@gmail.com>

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutcodelab/MainActivity.kt
@@ -77,6 +77,38 @@ fun LayoutsCodelab() {
 }
 
 @Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutCodelabTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
+
+@Composable
 fun LargeConstraintLayout() {
     ConstraintLayout {
         val text = createRef()
